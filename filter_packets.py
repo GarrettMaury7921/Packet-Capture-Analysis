2f02b8449006f9554c7da11d6d255d1b950acf89
Garrett Maury <90222973+GarrettMaury7921@users.noreply.github.com>
2021-11-29 20:47:02 -0500

    Filter Packets
    Loops through the 4 Node*.txts and grabs the ICMP echo request's and replies and outputs them to a separate Node*_filtered.txt

--- filter_packets.py
+++ filter_packets.py
@@ -1,3 +1,41 @@
 def filter():
-	print('called filter function in filter_packets.py')
-	
+	# Filter the raw text file (Node*.txt) so that only ICMP Echo Requests and Replies show
+	print("Filtering Packets...")
+	for i in [1, 2, 3, 4]:
+		print("    Filtering File " + str(i) + "...")
+		# Open The files
+		file = open("Node" + str(i) + ".txt", "r")
+		loop = True
+
+		while loop is True:
+			line1 = file.readline()  # Metric Headers
+			# If the file is no more
+			if line1 == "\n" or line1 == '':
+				loop = False
+
+			line2 = file.readline()  # Metrics
+			line3 = file.readline()  # Empty line
+
+			# FILTER THROUGH THE DATA
+			all_data = ''
+			data = True
+			while data is not False:
+				# Read in the data
+				data_line = file.readline()
+				all_data += data_line
+				if data_line == "\n" or data_line == '':
+					data = False  # End the loop
+
+			# OUTPUT ECHO REQUESTS AND REPLIES
+			# Continue only if the Metrics line includes ICMP Echo Requests and Replies
+			if "ICMP" in line2 and "(ping)" in line2 and "Echo" in line2:
+				if "request" in line2 or "reply" in line2:
+					# Put the Filtered packet into its own output file (Node*_filtered.txt)
+					output_file = open("Node" + str(i) + "_filtered.txt", "a")
+					output_file.write(line1)
+					output_file.write(line2)
+					output_file.write(line3)
+					output_file.write(all_data)
+	# Close files
+	file.close()
+	output_file.close()
