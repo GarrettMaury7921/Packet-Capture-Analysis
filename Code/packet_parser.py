e6693705c15b188f64c2ff10eebd42676bb7b6d4
ritvikpa1 <80577314+ritvikpa1@users.noreply.github.com>
2021-12-01 19:57:58 -0500

    parser working: tested

--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+import re
 def parse(filename, lst) :
+	temp=[]
 	with open(filename, "r") as file:
 		lines=file.readlines()
 		for line in lines:
-			lst.append(line.strip().split())
+			temp.append(line.strip().split())
+	
+	for k in temp:
+		if len(k)>2:
+			if re.search(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", k[2]):
+				lst.append(k)
