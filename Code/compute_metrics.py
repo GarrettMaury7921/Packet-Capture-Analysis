af738ff48cb5b3a0da8f816c381d5504d22181aa
Garrett Maury <90222973+GarrettMaury7921@users.noreply.github.com>
2021-12-03 18:05:14 -0500

    Data Sent/Received Metrics

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -32,25 +32,32 @@ def compute(lst1, lst2, lst3, lst4):
                 if i[8] == 'request':
                     req_sent = req_sent + 1
                     req_by_sent = req_by_sent + int(i[5])
+                    # only actual payload should be not frame length
+                    # DATA = Length - mac header, ip header, icmp header (42)
+                    req_da_sent = req_da_sent + int(i[5])
+                    req_da_sent = req_da_sent - 42
                 if i[8] == 'reply':
                     reply_sent = reply_sent + 1
-                    # only actual payload should be not frame length
-                    # req_da_sent = req_da_sent + int(i[5])
             # if not node data is received
             if i[2] != '192.168.100.1':
                 if i[8] == 'request':
                     req_rec = req_rec + 1
                     req_by_rec = req_by_rec + int(i[5])
+                    # only actual payload should be not frame length
+                    # DATA = Length - mac header, ip header, icmp header (42)
+                    req_da_rec = req_da_rec + int(i[5])
+                    req_da_rec = req_da_rec - 42
                 if i[8] == 'reply':
                     reply_rec = reply_rec + 1
-                    # only actual payload should be not frame length
-                    # req_da_rec = req_da_rec + int(i[5])
 
         # Print out what node is being printed
         print("NODE: " + str(node_num+1))
         print("Echo Requests Sent", req_sent, "\nEcho Requests Received", req_rec)
         print("Echo Replies Sent", reply_sent, "\nEcho Replies Received ", reply_rec)
+
         print("Echo Request Bytes Sent ", req_by_sent)
         print("Echo Request Bytes Received ", req_by_rec)
-        print(req_da_sent, req_da_rec, "\n\n")
+
+        print("Echo Request Data Sent ", req_da_sent)
+        print("Echo Request Data Received ", req_da_rec, "\n\n")
         node_num = node_num + 1
